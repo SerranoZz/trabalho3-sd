1b44db39ab7809f48722d9d22884d67da965d753
saramcav <saracavalcante@id.uff.br>
2024-06-20 16:12:18 -0300

    apagando funcoes nao usadas

--- API-RESTful-SB09-para-Projeto24-com-Infinity-Scroll-Completo/src/main/java/com/carlosribeiro/sb01/repository/NoticiaRepository.java
+++ API-RESTful-SB09-para-Projeto24-com-Infinity-Scroll-Completo/src/main/java/com/carlosribeiro/sb01/repository/NoticiaRepository.java
@@ -14,12 +14,6 @@ import java.util.List;
 
 
 public interface NoticiaRepository extends JpaRepository<Noticia, Long> {
-    @Query("SELECT n FROM Noticia n ORDER BY n.dataPostagem DESC")
-    List<Noticia> findAllByOrderByDataPostagemDesc();
-
-    @Query("SELECT n FROM Noticia n WHERE n.dataCriacao > :timestamp OR n.dataAtualizacao > :timestamp ORDER BY n.dataPostagem DESC")
-    List<Noticia> findByDataCriacaoAfterOrDataAtualizacaoAfterOrderByDataPostagemDesc(@Param("timestamp") LocalDateTime timestampAtualizacao);
-
     @Query("SELECT n FROM Noticia n order by n.dataPostagem desc")
     List<Noticia> recuperarNoticias();
 }
