e82ff3ee96cb3fe7ad639872d9d7613b90aa4aa7
saramcav <saracavalcante@id.uff.br>
2024-06-23 13:21:29 -0300

    impedindo que um adm visualize notificação de edição

--- src/components/ConfirmacaoNotificacao.tsx
+++ src/components/ConfirmacaoNotificacao.tsx
@@ -4,9 +4,12 @@ import { BASE_URL, BASE_URL2 } from '../util/constants';
 import { NotificacaoContext } from '../store/NotificacaoProvider';
 import Notificacao from '../interfaces/notificacao';
 import Swal from 'sweetalert2';
+import { useUsuarioContext } from '../store/UsuarioProvider';
 
 const ConfirmacaoNotificacao: React.FC = () => {
   const { ultimaNotificacao, setUltimaNotificacao } = useContext(NotificacaoContext);
+  const {estaLogado, admin} = useUsuarioContext();
+  
   useEffect(() => {
     const fetchUltimaNotificacao = async () => {
       if (Swal.isVisible()) { 
@@ -23,33 +26,52 @@ const ConfirmacaoNotificacao: React.FC = () => {
         const novaNotificacao = response.data;
 
         console.log("chegou", JSON.stringify(novaNotificacao))
-        
-        if (novaNotificacao.hasOwnProperty("operacao")) {
-          const mensagem = novaNotificacao.operacao === 'Cadastrada' ? 'Nova notícia cadastrada!' : novaNotificacao.operacao === 'Alterada' ? 'Notícia editada!' : 'Notícia excluída!';
-          console.log(novaNotificacao.operacao);
-          if(novaNotificacao.operacao != 'Removida') {
-            Swal.fire({
-              title: mensagem,
-              text: `Deseja visualizar a notícia "${novaNotificacao.titulo}"?`, 
-              icon: 'info',
-              showCancelButton: true,
-              confirmButtonText: 'Sim',
-              cancelButtonText: 'Não',
-              confirmButtonColor: '#FFD700', 
-              cancelButtonColor: '#FFD700',
-            }).then((result) => {
-              if (result.isConfirmed) {
-                window.location.href = `${BASE_URL2}/noticia/${novaNotificacao.id}`;
-              }
-            });
-          } 
-          
+
+        if (!novaNotificacao.hasOwnProperty("operacao")) {
+          return;
+        }
+
+        if(novaNotificacao.operacao === 'Alterada' && admin()) {
           const novoValor: Notificacao = { 
             id: novaNotificacao.id, 
             timestamp: novaNotificacao.timestamp
           };
           setUltimaNotificacao(novoValor);
+          return;
         }
+
+        if(novaNotificacao.operacao === 'Removida') {
+          const novoValor: Notificacao = { 
+            id: novaNotificacao.id, 
+            timestamp: novaNotificacao.timestamp
+          };
+          setUltimaNotificacao(novoValor);
+          window.location.reload();
+          return;
+        }
+        
+        const mensagem = novaNotificacao.operacao === 'Cadastrada' ? 'Nova notícia cadastrada!' : novaNotificacao.operacao === 'Alterada' ? 'Notícia editada!' : 'Notícia excluída!';
+        
+        Swal.fire({
+          title: mensagem,
+          text: `Deseja visualizar a notícia "${novaNotificacao.titulo}"?`, 
+          icon: 'info',
+          showCancelButton: true,
+          confirmButtonText: 'Sim',
+          cancelButtonText: 'Não',
+          confirmButtonColor: '#FFD700', 
+          cancelButtonColor: '#FFD700',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = `${BASE_URL2}/noticia/${novaNotificacao.id}`;
+          }
+        });
+        
+        const novoValor: Notificacao = { 
+          id: novaNotificacao.id, 
+          timestamp: novaNotificacao.timestamp
+        };
+        setUltimaNotificacao(novoValor);
       } catch (error) {
         console.error('Erro ao buscar notificação:', error);
       }
@@ -64,4 +86,3 @@ const ConfirmacaoNotificacao: React.FC = () => {
 };
 
 export default ConfirmacaoNotificacao;
-
